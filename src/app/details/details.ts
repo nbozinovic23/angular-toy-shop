7e372e7fe5c89ddc959bae52611d32de53c47ddf
nbozinovic23 <nemanja.bozinovic.23@singimail.rs>
2026-03-11 19:43:12 +0100

    Updated details component layout

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,21 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ToyModel } from '../../models/toy.model';
+import { Utils } from '../utils';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import axios from 'axios';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
 export class Details {
   toy = signal<ToyModel | null>(null)
 
-  constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute, public utils: Utils) {
     route.params.subscribe(params => {
       const id = params['id']
-      axios.get(`https://toy.pequla.com/api/toy/${id}`)
+      axios.get<ToyModel>(`https://toy.pequla.com/api/toy/${id}`)
         .then(rsp => this.toy.set(rsp.data))
     })
   }
